c0d092d9245f864e2de6576104e72c7bed6f4a33
Pratik Dashore <pratik.dashore@accolitelabs.com>
2019-05-13 12:12:59 +0530

    removing errors

--- rxjs/14.operators_error.ts
+++ rxjs/14.operators_error.ts
@@ -1,28 +1,20 @@
-import { of } from 'rxjs';
-import { catchError, map, throwIfEmpty } from 'rxjs/operators';
-
-of('a', 10)
-  .pipe(
-    map(v => v.toUpperCase()),
-    catchError(err => of(undefined)),
-  )
-  .subscribe(
-    res => {
-      if (res) {
-        console.log(res);
-      }
-    },
-    err => console.log(err),
-    () => console.log('complete')
-  );
-
+// of('a', 10).pipe(
+//   map(v => v.toUpperCase()),
+//   catchError(err => of(undefined))
+// );
+// .subscribe(
+//   res => {
+//     if (res) {
+//       console.log(res);
+//     }
+//   },
+//   err => console.log(err),
+//   () => console.log('complete')
+// );
 
 // throwIfEmpty -> if nothing gets emmited and observable gets complete it throws error
 // onErrorResumeNext -> will switch to new observable stream if error occurs
-// retry 
-// retryWhen 
+// retry
+// retryWhen
 // timeout
 // timeoutWith
-
-
-
